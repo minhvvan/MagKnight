e4578c0cc98a0a6a571f1ceaf88d1dd103e52324
jinubee12 <jinubee12@naver.com>
2025-04-15 10:58:21 +0900

    롤백

--- Assets/Scripts/Player/AttackType.cs
+++
@@ -1,7 +0,0 @@
-// AttackType.cs
-public enum AttackType
-{
-    Weak,
-    Strong,
-    // 필요한 만큼 추가 가능
-}
--- Assets/Scripts/Player/ComboBranch.cs
+++
@@ -1,9 +0,0 @@
-// ComboBranch.cs
-using UnityEngine;
-
-[System.Serializable]
-public struct ComboBranch
-{
-    public AttackType inputType;  // 예: 약(Weak)인지 강(Strong)인지
-    public int nextStepIndex;     // 만약 약이면 nextStepIndex 번 스텝으로 이동
-}
