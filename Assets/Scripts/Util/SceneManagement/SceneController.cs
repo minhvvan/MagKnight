311a0a5f2252afe7b43ec45ad1565a292aa90b97
moon <ediamoon@naver.com>
2025-04-17 01:33:58 +0900

    [Feat] Slash to game 기본 씬 전환 코드 추가

---
+++ Assets/Scripts/Util/SceneManagement/SceneController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Moon;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace Moon
+{
+    public class SceneController : Singleton<SceneController>
+    {
+        //기존 샘플 코드에서는 TriggerEvent의 중복을 막기 위하여 사용했던것으로 보임
+        public static bool Transitioning
+        {
+            get { return Instance._transitioning; }
+        }
+          
+        protected InputHandler _inputHandler;
+        protected bool _transitioning;
+
+        void Start()
+        {
+            _inputHandler = FindObjectOfType<InputHandler>();  
+        }
+    
+        public static void TransitionToScene(string sceneName)
+        {
+            Instance.StartCoroutine(Instance.Transition(sceneName));
+        }
+
+        protected IEnumerator Transition(string newSceneName)
+        {
+            _transitioning = true;
+
+            if (_inputHandler == null)
+                _inputHandler = FindObjectOfType<InputHandler>();
+            if (_inputHandler) _inputHandler.ReleaseControl();
+            yield return StartCoroutine(ScreenFader.FadeSceneOut(ScreenFader.FadeType.Loading));
+            yield return SceneManager.LoadSceneAsync(newSceneName);
+            _inputHandler = FindObjectOfType<InputHandler>();
+            if (_inputHandler) _inputHandler.ReleaseControl();
+            yield return StartCoroutine(ScreenFader.FadeSceneIn());
+            if (_inputHandler)
+                _inputHandler.GainControl();
+
+            _transitioning = false;
+        }
+
+        IEnumerator CallWithDelay(float delay, Action call)
+        {
+            yield return new WaitForSeconds(delay);
+            call();
+        }
+    }
+}
---
+++ Assets/Scripts/Util/SceneManagement/ScreenFader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Moon
+{
+    public class ScreenFader : Singleton<ScreenFader>
+    {
+        public enum FadeType
+        {
+            CommonFade, Loading, Intro,
+        }
+
+        public static bool IsFading
+        {
+            get { return Instance._isFading; }
+        }
+
+        [SerializeField] public CanvasGroup commonFaderCanvasGroup;
+        [SerializeField] public CanvasGroup loadingCanvasGroup;
+        [SerializeField] public float fadeDuration = 1f;
+
+        protected bool _isFading;
+        const int k_MaxSortingLayer = 32767;
+
+        protected IEnumerator Fade(float finalAlpha, CanvasGroup canvasGroup)
+        {
+            _isFading = true;
+            canvasGroup.blocksRaycasts = true;
+            float fadeSpeed = Mathf.Abs(canvasGroup.alpha - finalAlpha) / fadeDuration;
+            while (!Mathf.Approximately(canvasGroup.alpha, finalAlpha))
+            {
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, finalAlpha,
+                    fadeSpeed * Time.deltaTime);
+                yield return null;
+            }
+            canvasGroup.alpha = finalAlpha;
+            _isFading = false;
+            canvasGroup.blocksRaycasts = false;
+        }
+
+        public static void SetAlpha(float alpha)
+        {
+            Instance.commonFaderCanvasGroup.alpha = alpha;
+        }
+
+        
+        public static IEnumerator FadeSceneIn()
+        {
+            CanvasGroup canvasGroup;
+            if (Instance.commonFaderCanvasGroup.alpha > 0.1f)
+                canvasGroup = Instance.commonFaderCanvasGroup;
+            else
+                canvasGroup = Instance.loadingCanvasGroup;
+
+            yield return Instance.StartCoroutine(Instance.Fade(0f, canvasGroup));
+
+            canvasGroup.gameObject.SetActive(false);
+        }
+
+        public static IEnumerator FadeSceneOut(FadeType fadeType = FadeType.CommonFade)
+        {
+            CanvasGroup canvasGroup;
+            switch (fadeType)
+            {
+                case FadeType.CommonFade:
+                    canvasGroup = Instance.commonFaderCanvasGroup;
+                    break;
+                default:
+                    canvasGroup = Instance.loadingCanvasGroup;
+                    break;
+            }
+
+            canvasGroup.gameObject.SetActive(true);
+
+            yield return Instance.StartCoroutine(Instance.Fade(1f, canvasGroup));
+        }
+    }
+}
