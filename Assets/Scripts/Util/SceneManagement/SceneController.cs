e5f93dc1d19cac87f966dd6f3489c55edfd850d7
moon <ediamoon@naver.com>
2025-04-17 15:00:31 +0900

    [Resource] 폰트 추가 및 테스트 베이스 캠프 제작

--- Assets/Scripts/Util/SceneManagement/SceneController.cs
+++ Assets/Scripts/Util/SceneManagement/SceneController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using Moon;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 namespace Moon
