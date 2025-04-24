c19d0db4d2a38da14c64721cc9a7b76ae15c3ce7
oriorori <junha3679@gmail.com>
2025-04-24 17:37:54 +0900

    [Feat] 무기 없을 때 player Anim 구현

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using AYellowpaper.SerializedCollections;
 using Cinemachine;
 using Cysharp.Threading.Tasks;
 using hvvan;
@@ -33,6 +34,8 @@ namespace Moon
 
         public CameraSettings cameraSettings;
         public bool isDead;
+        
+        [SerializeField] private SerializedDictionary<WeaponType, RuntimeAnimatorController> animatorControllers;
 
         LockOnSystem _lockOnSystem;
         bool _lockOnLastFrame = false;
@@ -704,7 +707,9 @@ namespace Moon
         #region Weapon
         public void SetCurrentWeapon(WeaponType weaponType)
         {
+            _animator.runtimeAnimatorController = animatorControllers[weaponType];
             _weaponHandler.SetCurrentWeapon(weaponType);
+            canAttack = true;
         }
         #endregion
         
