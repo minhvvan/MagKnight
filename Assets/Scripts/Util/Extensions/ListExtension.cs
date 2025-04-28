61f419513911b89d03a227aa6e15b3cdd0070e74
hvvan <eminhwan12@gmail.com>
2025-04-28 14:55:30 +0900

    [Fix] 클리어 룸 gate 미생성 버그 수정

---
+++ Assets/Scripts/Util/Extensions/ListExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListExtensions
+{
+    public static void AddUnique<T>(this List<T> list, T newItem)
+    {
+        if (list == null) return;
+        if(list.Contains(newItem)) return;
+        
+        list.Add(newItem);
+    }
+}
