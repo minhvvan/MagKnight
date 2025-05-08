f8f49d15247e5e9ef753ba4a3a1da55a80c4dbbe
moon <ediamoon@naver.com>
2025-05-09 02:08:40 +0900

    [Feat] 로딩UI 변경, 인트로 이어하기, 처음부터 추가, 타이틀 상태에서 세이브 데이터 로드 하도록 수정

--- Assets/Scripts/UI/IntroUIController.cs
+++ Assets/Scripts/UI/IntroUIController.cs
@@ -1,5 +1,7 @@
+using System;
 using DG.Tweening;
 using hvvan;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,12 +9,53 @@ namespace Moon
 {
     public class IntroUIController : MonoBehaviour
     {
+        [SerializeField] TextMeshProUGUI _pressAnyKeyText;
+        [SerializeField] RectTransform _buttonRectTransform;
+        [SerializeField] CanvasGroup _canvasGroup;
+        [SerializeField] Button _continueButton;
+        [SerializeField] Button _restartButton;
+
+        void Awake()
+        {
+            _continueButton.onClick.AddListener(OnClickContinue);
+            _restartButton.onClick.AddListener(OnClickRestart);
+
+            _buttonRectTransform.gameObject.SetActive(false);
+            _pressAnyKeyText.gameObject.SetActive(true);
+        }
+
+        private void OnClickContinue()
+        {
+            GameManager.Instance.ChangeGameState(GameState.InitGame);
+        }
+
+        private void OnClickRestart()
+        {
+            GameManager.Instance.DeleteData(Constants.CurrentRun);
+            GameManager.Instance.SaveData(Constants.PlayerData);
+            GameManager.Instance.ChangeGameState(GameState.InitGame);
+        }
+
         void Update()
         {
-            if (Input.anyKeyDown)
+            if(_pressAnyKeyText.gameObject.activeSelf)
             {
-                DOTween.KillAll();
-                GameManager.Instance.ChangeGameState(GameState.InitGame);
+                if (Input.anyKeyDown)
+                {
+                    DOTween.KillAll();
+                    _pressAnyKeyText.gameObject.SetActive(false);
+                    _buttonRectTransform.gameObject.SetActive(true);
+
+                    if(GameManager.Instance.CurrentRunData == null || !GameManager.Instance.CurrentRunData.isDungeonEnter)
+                    {
+                        _continueButton.gameObject.SetActive(false);
+                    }
+
+                    _canvasGroup.DOFade(1f, 0.5f);
+
+                    //Cursor visible
+                    UIManager.Instance.EnableCursor();
+                }
             }
         }
     }
