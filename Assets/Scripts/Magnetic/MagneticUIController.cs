7866c5e967a78b3d4a28b65c6cdbc6894a576427
ToastBox <car9245@naver.com>
2025-05-09 11:30:54 +0900

    [Feat] MagneticUI 이미지 리소스 변경, 애니메이션 및 연출 추가.

--- Assets/Scripts/Magnetic/MagneticUIController.cs
+++ Assets/Scripts/Magnetic/MagneticUIController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,29 +7,62 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
+[Serializable]
+public struct MagnetUIColorGroup
+{
+    public Color32[] colors;
+}
+
 public class MagneticUIController : MonoBehaviour
 {
     [Header("Target Prefab")]
     public MagneticTarget magneticTargetPrefab; //타겟 프리팹
     
-    [Header("Ready Container")]
-    public Transform imageContainer;
-    public int poolSize = 20;
+    [Header("TypeVisual Aim")]
+    public Image magneticTypeVisualAim;
+    
+    [Header("Traking Aim")]
+    public RectTransform trakingCircleRect;
+    public Image trakingCircleImage;
+    public Vector2 trakingCircleOrigin;
+    public float trakerRotateSpeed;
+    
+    private CanvasGroup _trakingCircleCanvasGroup;
+    private Image[] _trackingGroupImages;
+    private bool _isRotate = false;
+    
+    [Header("SideTri Aim")]
+    public RectTransform triAimLeftRectTr;
+    public RectTransform triAimRightRectTr;
+    public float triAimOffset;
+    public float triAimOrigin;
     
     [Header("FocusAreaCircle")]
     public GameObject focusAreaCircle;
     private CanvasGroup _focusCircleCanvasGroup;
     
+    [Header("CoolDown")]
+    public GameObject coolDownPanel;
+    private CanvasGroup _coolDownCanvasGroup;
+    public Image coolDownIcon;
+    private CanvasGroup _coolDownIconCanvasGroup;
+    public Image coolDownFill;
+    public float iconAlphaLow;
+    public float fillLowValue;
+    
     [Header("MagneticTypeVisual")]
-    public GameObject magneticTypeN;
-    public GameObject magneticTypeS;
-    private CanvasGroup _typeNCanvasGroup;
-    private CanvasGroup _typeSCanvasGroup;
+    public List<MagnetUIColorGroup> circleUIColors;
+    private Image[] _magneticTypeImages;
+    
+    [Header("Ready Container")]
+    public Transform imageContainer;
+    public int poolSize = 20;
     
     private Queue<MagneticTarget> _targetImgPool = new Queue<MagneticTarget>(); // 타겟 이미지 풀링
     private List<MagneticTarget> _currentTargetList = new List<MagneticTarget>(); //화면 내에 표기되는 타겟 리스트
 
-    private WaitForSeconds _waitDoTween;
+    private WaitForSecondsRealtime _waitDoTween;
+    private Coroutine _hideCoolDownUICoroutine;
     
     private bool _isDispose = false; //true 시 모든 동작 중단.
     private float _dotDuration;
@@ -49,122 +83,190 @@ public class MagneticUIController : MonoBehaviour
     private void InitializeUI()
     {
         _focusCircleCanvasGroup = focusAreaCircle.GetComponent<CanvasGroup>();
-        _typeNCanvasGroup = magneticTypeN.GetComponent<CanvasGroup>();
-        _typeSCanvasGroup = magneticTypeS.GetComponent<CanvasGroup>();
+        _coolDownCanvasGroup = coolDownPanel.GetComponent<CanvasGroup>();
+        _coolDownIconCanvasGroup = coolDownIcon.GetComponent<CanvasGroup>();
+        _trakingCircleCanvasGroup = trakingCircleImage.GetComponent<CanvasGroup>();
+        
+        _magneticTypeImages = focusAreaCircle.GetComponentsInChildren<Image>();
+        _trackingGroupImages = trakingCircleRect.GetComponentsInChildren<Image>();
         
         _focusCircleCanvasGroup.alpha = 0;
-        _typeNCanvasGroup.alpha = 0;
-        _typeSCanvasGroup.alpha = 0;
+        _trakingCircleCanvasGroup.alpha = 0;
+        _coolDownCanvasGroup.alpha = 0;
         
         focusAreaCircle.transform.DOScale(Vector3.zero, 0);
-        magneticTypeN.transform.DOScale(Vector3.zero, 0);
-        magneticTypeS.transform.DOScale(Vector3.zero, 0);
+        trakingCircleRect.transform.DOScale(Vector3.zero, 0);
+        trakingCircleOrigin = trakingCircleRect.anchoredPosition;
 
         _dotDuration = 0.2f;
-        _waitDoTween = new WaitForSeconds(_dotDuration);
+        _waitDoTween = new WaitForSecondsRealtime(_dotDuration);
     }
     
     public IEnumerator ShowFocusArea()
     {
-        // focusAreaCircle.transform.DOScale(Vector3.one, _dotDuration);
-        // _focusCircleCanvasGroup.DOFade(1, _dotDuration);
         StartCoroutine(UIScale(focusAreaCircle.transform, Vector3.one, _dotDuration));
         StartCoroutine(UIFade(_focusCircleCanvasGroup, 1f, _dotDuration));
+        StartCoroutine(UIScale(trakingCircleRect.transform, Vector3.one, _dotDuration));
+        StartCoroutine(UIFade(_trakingCircleCanvasGroup, 1f, _dotDuration));
+        StartCoroutine(UIScale(coolDownPanel.transform, Vector3.one, _dotDuration));
+        
+        StartCoroutine(SetRotateCircle());
         
         yield return _waitDoTween;
     }
 
     public IEnumerator HideFocusArea()
     {
-        // focusAreaCircle.transform.DOScale(Vector3.zero, _dotDuration);
-        // _focusCircleCanvasGroup.DOFade(0, _dotDuration);
-        
         StartCoroutine(UIScale(focusAreaCircle.transform, Vector3.zero, _dotDuration));
         StartCoroutine(UIFade(_focusCircleCanvasGroup, 0f, _dotDuration));
+        StartCoroutine(UIScale(trakingCircleRect.transform, Vector3.zero, _dotDuration));
+        StartCoroutine(UIFade(_trakingCircleCanvasGroup, 0f, _dotDuration));
+        StartCoroutine(UIScale(coolDownPanel.transform, Vector3.zero, _dotDuration));
+        
+        _isRotate = false;
         yield return _waitDoTween;
     }
 
-    public IEnumerator ShowMagneticTypeVisual(MagneticType type)
+    //중앙 원형에임 UI의 락온 여부에 따른 색상을 변경합니다.
+    public IEnumerator SetTargetMagneticTypeColor(MagneticType? type = null)
     {
-        switch (type)
+        if (type.HasValue)
         {
-            case MagneticType.N:
-                if (_typeNCanvasGroup.alpha == 0 && _typeSCanvasGroup.alpha == 0)
-                {
-                    // magneticTypeN.transform.DOScale(Vector3.one, _dotDuration);
-                    // magneticTypeS.transform.DOScale(Vector3.one, _dotDuration);
-                    // _typeNCanvasGroup.DOFade(1, _dotDuration);
-                    StartCoroutine(UIScale(magneticTypeN.transform, Vector3.one, _dotDuration));
-                    StartCoroutine(UIScale(magneticTypeS.transform, Vector3.one, _dotDuration));
-                    StartCoroutine(UIFade(_typeNCanvasGroup, 1f, _dotDuration));
-                    
-                    yield return _waitDoTween;
-                    
-                    yield break;
-                }
-                _typeNCanvasGroup.DOFade(1, 0);
-                _typeSCanvasGroup.DOFade(0, 0);
-                break;
-            case MagneticType.S:
-                if (_typeNCanvasGroup.alpha == 0 && _typeSCanvasGroup.alpha == 0)
-                {
-                    // magneticTypeN.transform.DOScale(Vector3.one, _dotDuration);
-                    // magneticTypeS.transform.DOScale(Vector3.one, _dotDuration);
-                    // _typeSCanvasGroup.DOFade(1, _dotDuration);
-                    StartCoroutine(UIScale(magneticTypeN.transform, Vector3.one, _dotDuration));
-                    StartCoroutine(UIScale(magneticTypeS.transform, Vector3.one, _dotDuration));
-                    StartCoroutine(UIFade(_typeSCanvasGroup, 1f, _dotDuration));
-                    
-                    yield return _waitDoTween;
-                    
-                    yield break;
-                }
-                _typeNCanvasGroup.DOFade(0, 0);
-                _typeSCanvasGroup.DOFade(1, 0);
-                break;
+            var colorGroup = circleUIColors[2];
+            trakingCircleImage.color = colorGroup.colors[0];
+        }
+        else
+        {
+            trakingCircleImage.color  = Color.white;
+        }
+        
+        yield break;
+    }
+
+    //중앙 원형에임 UI를 회전시킵니다.
+    public IEnumerator SetRotateCircle()
+    {
+        if (_isRotate) yield break;
+        _isRotate = true;
+        var angle = 0f;
+        while (_isRotate)
+        {
+            angle = trakerRotateSpeed * Time.unscaledDeltaTime;
+            trakingCircleImage.transform.Rotate(Vector3.forward, angle);
+            yield return null;
         }
     }
 
-    public IEnumerator HideMagneticTypeVisual()
+    //락온 대상을 추적하는 < > 형태의 에임의 상황별 위치를 조정합니다.
+    public IEnumerator SetTriAimPosition(bool isOn)
     {
-        // magneticTypeN.transform.DOScale(Vector3.zero, _dotDuration);
-        // magneticTypeS.transform.DOScale(Vector3.zero, _dotDuration);
-        //
-        // _typeNCanvasGroup.DOFade(0, _dotDuration);
-        // _typeSCanvasGroup.DOFade(0, _dotDuration);
+        if (isOn) //락온 시
+        {
+            triAimLeftRectTr.anchoredPosition = new Vector2(-triAimOffset, 0);
+            triAimRightRectTr.anchoredPosition = new Vector2(triAimOffset, 0);
+        }
+        else //평시 상태로 돌아올 시
+        {
+            triAimLeftRectTr.anchoredPosition = new Vector2(-triAimOrigin, 0);
+            triAimRightRectTr.anchoredPosition = new Vector2(triAimOrigin, 0);
+        }
         
-        StartCoroutine(UIScale(magneticTypeN.transform, Vector3.zero, _dotDuration));
-        StartCoroutine(UIScale(magneticTypeS.transform, Vector3.zero, _dotDuration));
-        StartCoroutine(UIFade(_typeNCanvasGroup, 0, _dotDuration));
-        StartCoroutine(UIFade(_typeSCanvasGroup, 0, _dotDuration));
+        yield break;
+    }
 
-        yield return _waitDoTween;
+    //전체 MagneticAim컬러를 일괄 설정합니다.
+    public IEnumerator SetMagneticTypeVisual(MagneticType type)
+    {
+        var colorGroup = circleUIColors[(int)type];
+        magneticTypeVisualAim.color = colorGroup.colors[0];
+        for (int i = 0; i < _magneticTypeImages.Length; i++)
+        {
+            //if (i != 0) trackingGroupImages[i].color = colorGroup.colors[i];
+            _magneticTypeImages[i].color = i == 0 ? colorGroup.colors[1] : colorGroup.colors[0];
+        }
+        yield break;
+    }
+    
+    //극성 전환 후, 다음 재사용까지의 대기시간을 쿨타임UI로 알려줍니다.
+    public IEnumerator MagneticSwitchCoolDownVisual(float duration)
+    {
+        if (_hideCoolDownUICoroutine != null)
+        {
+            StopCoroutine(_hideCoolDownUICoroutine);
+            _hideCoolDownUICoroutine = null;
+        }
+        
+        _coolDownIconCanvasGroup.alpha = iconAlphaLow;
+        StartCoroutine(UIFade(_coolDownCanvasGroup, 1f, _dotDuration));
+        var elapsedTime = 0f;
+        coolDownFill.fillAmount = fillLowValue;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            coolDownFill.fillAmount = Mathf.Lerp(fillLowValue, 1f, (elapsedTime / duration));
+            yield return null;
+        }
+        _hideCoolDownUICoroutine = StartCoroutine(UIFade(_coolDownIconCanvasGroup, 1f, _dotDuration, () =>
+        {
+            StartCoroutine(UIFade(_coolDownCanvasGroup, 0f, _dotDuration));
+        }));
+    }
+
+    //극성전환 시 MagneticAim을 좌우로 회전시킵니다.
+    public IEnumerator SwitchMagneticTypeVisual(MagneticType type)
+    {
+        StartCoroutine(UIRotate(focusAreaCircle.transform, -45f, 0.2f, () =>
+        {
+            StartCoroutine(SetMagneticTypeVisual(type));
+            StartCoroutine(UIRotate(focusAreaCircle.transform, 45f, 0.2f));
+        }));
+        yield break;
     }
 
     //DOTween은 TimeScale이 느려지면 같이 느려진다. 코루틴은 그것에 영향을 받지 않는다.
     
     //TimeScale에 영향을 받지 않는 DOTween DOScale!
-    public IEnumerator UIScale(Transform target ,Vector3 scale, float duration)
+    public IEnumerator UIScale(Transform target ,Vector3 scale, float duration, Action onComplete = null)
     {
         var elapsedTime = 0f;
         while (elapsedTime < duration)
         {
-            elapsedTime += Time.deltaTime;
+            elapsedTime += Time.unscaledDeltaTime;
             target.localScale = Vector3.Lerp(target.localScale, scale, elapsedTime / duration);
             yield return null;
         }
+        onComplete?.Invoke();
     }
 
     //TimeScale에 영향을 받지 않는 DOTween DOFade!
-    public IEnumerator UIFade(CanvasGroup target, float value, float duration)
+    public IEnumerator UIFade(CanvasGroup target, float value, float duration,  Action onComplete = null)
     {
         var elapsedTime = 0f;
         while (elapsedTime < duration)
         {
-            elapsedTime += Time.deltaTime;
+            elapsedTime += Time.unscaledDeltaTime;
             target.alpha = Mathf.Lerp(target.alpha, value, elapsedTime / duration);
             yield return null;
         }
+        onComplete?.Invoke();
+    }
+    
+    //TimeScale에 영향을 받지 않는 DOTween DORotate!
+    public IEnumerator UIRotate(Transform target, float angle, float duration,  Action onComplete = null)
+    {
+        var elapsedTime = 0f;
+        var targetAngle = new Vector3(target.localEulerAngles.x, target.localEulerAngles.y, target.localEulerAngles.z);
+        var finishAngle = new Vector3(targetAngle.x, targetAngle.y, targetAngle.z + angle);
+        
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            targetAngle = new Vector3(target.localEulerAngles.x, target.localEulerAngles.y, target.localEulerAngles.z);
+            target.localRotation = Quaternion.Lerp(Quaternion.Euler(targetAngle), Quaternion.Euler(finishAngle), elapsedTime / duration);
+            
+            yield return null;
+        }
+        onComplete?.Invoke();
     }
 
     #endregion
@@ -253,9 +355,10 @@ public class MagneticUIController : MonoBehaviour
     public void InLockOnTarget(Transform target)
     {
         if (_isDispose) return;
-        
+        StartCoroutine(SetTriAimPosition(true));
         foreach (var targetObj in _currentTargetList.Where(targetObj => targetObj.target == target))
         {
+            targetObj.onTrakingCirclePos = SetTrakingCircle;
             targetObj.LockTarget();
         }
     }
@@ -264,10 +367,14 @@ public class MagneticUIController : MonoBehaviour
     public void UnLockOnTarget(Transform target)
     {
         if (_isDispose) return;
-        
+        StartCoroutine(SetTargetMagneticTypeColor());
+        StartCoroutine(SetTriAimPosition(false));
         foreach (var targetObj in _currentTargetList.Where(targetObj => targetObj.target == target))
         {
+            targetObj.onTrakingCirclePos = null;
+            trakingCircleRect.anchoredPosition = trakingCircleOrigin;
             targetObj.UnlockTarget();
+            
         }
     }
     
@@ -278,6 +385,11 @@ public class MagneticUIController : MonoBehaviour
         {
             targetObj.LostTarget();
         }
+    }
+
+    public void SetTrakingCircle(Vector2 pos)
+    {
+        trakingCircleRect.anchoredPosition = pos;
     }
     
     #endregion
