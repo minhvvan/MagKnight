43fd847ef4b81b06fcc6d864d4f920b1036b0fd8
hvvan <eminhwan12@gmail.com>
2025-05-09 16:30:52 +0900

    [Feat] 게임 클리어 로직 구현
    - GameClear State에서 처리

--- Assets/Scripts/Manager/GameStates/GameClearState.cs
+++ Assets/Scripts/Manager/GameStates/GameClearState.cs
@@ -1,9 +1,18 @@
 ﻿
+using hvvan;
+
 public class GameClearState: IGameState
 {
-    public void OnEnter()
+    public async void OnEnter()
     {
         //TODO: 클리어 연출(엔딩 크래딧?)
+        
+        //데이터 관리(current삭제 및 재화 저장)
+        GameManager.Instance.DeleteData(Constants.CurrentRun);
+        await GameManager.Instance.SaveData(Constants.PlayerData);
+        
+        //베이스 캠프로 이동
+        GameManager.Instance.ChangeGameState(GameState.InitGame);
     }
 
     public void OnUpdate()
