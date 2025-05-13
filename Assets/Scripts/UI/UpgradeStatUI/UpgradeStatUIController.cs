090f3a3f1c072f38599fdd6a874f8bfdd233632b
lmj0927 <lmjj0927@naver.com>
2025-05-13 11:17:27 +0900

    [Feat] Currency 로직 변경 및 Result 재화 반영 문제 수정

--- Assets/Scripts/UI/UpgradeStatUI/UpgradeStatUIController.cs
+++ Assets/Scripts/UI/UpgradeStatUI/UpgradeStatUIController.cs
@@ -116,7 +116,7 @@ public class UpgradeStatUIController : MonoBehaviour, IBasePopupUIController
         await GameManager.Instance.SaveData(Constants.PlayerData);
         await GameManager.Instance.SaveData(Constants.CurrentRun);
         
-        UIManager.Instance.inGameUIController.currencyUIController.UpdateUI();
+        UIManager.Instance.inGameUIController.currencyUIController.UpdateCurrency(_playerData);
         HideUI();
     }
 }
