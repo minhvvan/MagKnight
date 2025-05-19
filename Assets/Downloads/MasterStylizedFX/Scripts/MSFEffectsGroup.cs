34f0ed8a651fb8ced4378be6259214e2cd2f15de
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-05-19 12:27:44 +0900

    Delete Assets/Downloads directory

--- Assets/Downloads/MasterStylizedFX/Scripts/MSFEffectsGroup.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "MasterStylizedFX/EffectGroup")]
-public class MSFEffectsGroup : ScriptableObject
-{
-    public List<ParticleSystem> Particles;
-}
